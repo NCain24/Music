ad99ff2db92db130c23e95f7d1f250bae44ccb07
Noah Cain <88724898+NCain24@users.noreply.github.com>
2023-07-23 08:43:12 -0700

    Added Search function

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import SupabaseProvider from '@/providers/SupabaseProvider';
 import UserProvider from '@/providers/UserProvider';
 import ModalProvider from '@/providers/ModalProvider';
 import ToasterProvider from '@/providers/ToasterProvider';
+import getSongsByUserId from '@/actions/getSongsByUserId';
 
 const font = Quicksand({ subsets: ['latin'], weight: '300' });
 
@@ -13,20 +14,23 @@ export const metadata: Metadata = {
   title: 'Music',
   description: 'Created by Noah Cain',
 };
-
-export default function RootLayout({
+export const revalidate = 0;
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const userSongs = await getSongsByUserId();
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
-            <ModalProvider/>
-          <Sidebar>{children}</Sidebar></UserProvider>
+            <ModalProvider />
+            <Sidebar songs={userSongs}>{children}</Sidebar>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
